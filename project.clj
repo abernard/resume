7d7c5daeee4c6c42aa7af60e6fe469b87def3efa
Aaron Bernard <aaron.bernard@duramec.com>
2015-02-14 22:03:36 -0600

    update project dependencies

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject resume "0.0.1"
+(defproject resume "0.1.0"
             :descripton "Resume generator"
             :url "http://zig.io"
             :plugins [[lein-cljsbuild "0.3.1"]
                       [lein-npm "0.1.0"]]
             :dependencies [[org.clojure/clojure "1.5.1"]
-                           [hiccups "0.2.0"]]
-            :node-dependencies [[cheerio "0.12.0"]
-                                [express "3.3.4"]
-                                [mustache "0.7.2"]
-                                [bootstrap "0.0.2"]
-                                [pdfkit "0.2.0"]
-                                [less-middleware "0.1.12"]]
+                           [hiccups "0.3.0"]]
+            :node-dependencies [[cheerio "0.18.0"]
+                                [express "4.11.2"]
+                                [mustache "1.0.0"]
+                                [bootstrap "3.3.2"]
+                                [pdfkit "0.7.0"]
+                                [less-middleware "1.0.4"]]
             :profiles {:dev {:cljsbuild {:builds [{:id "development"
                                                    :source-paths ["src/cljs"]
                                                    :compiler {
